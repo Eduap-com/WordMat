b2a622f164447a9816dec3d81a7b93d5041ec85c
Mikael Samsøe Sørensen <eduap@eduap.com>
2020-03-08 18:03:52 +0100

    Mac folder reorder

---
+++ Mac/Installer-files/InstallScript.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# now created using installer
+# mkdir -p /Library/Application\ support/Microsoft/Office365/User\ content.localized/Add-ins.localized/WordMat/
+# mkdir -p /Library/Application\ Support/Microsoft/Office365/User\ content.localized/Startup.localized/Word/
+# mv /Applications/WordMat2016/WordMatWinMac.dotm /Library/Application\ support/Microsoft/Office365/User\ content.localized/Startup.localized/Word/
+# mv /Applications/WordMat2016/LibMaximaConnection.dylib /Library/Application\ support/Microsoft/Office365/User\ content.localized/Add-ins.localized/WordMat/
+# mv /Applications/WordMat2016/maxima.app /Library/Application\ support/Microsoft/Office365/User\ content.localized/Add-ins.localized/WordMat/
+# mv /Applications/WordMat2016/gnuplot /Library/Application\ support/Microsoft/Office365/User\ content.localized/Add-ins.localized/WordMat/
+
+# Create the temp folder
+#mkdir -p ~/Library/Containers/com.microsoft.word/Data/WordMat/
+
+# the scriptfile for applescripttask
+mkdir -p ~/Library/Application\ Scripts/com.microsoft.Word/
+mv /Applications/WordMat2016/WordMatScripts.scpt ~/Library/Application\ scripts/com.microsoft.word/
+
+#mkdir -p ~/Library/Containers/com.microsoft.Word/Data/WordMat/Formulae/
+
+#cp -R /Library/Application\ support/Microsoft/Office365/User\ content.localized/Add-ins.localized/WordMat/Formulae/ ~/Library/Containers/com.microsoft.Word/Data/WordMat/Formulae/
+
+#cp  /Library/Application\ support/Microsoft/Office365/User\ content.localized/Add-ins.localized/WordMat/Figurer.docx ~/Library/Containers/com.microsoft.Word/Data/WordMat/
+
+#cp  /Library/Application\ support/Microsoft/Office365/User\ content.localized/Add-ins.localized/WordMat/Figurer_english.docx ~/Library/Containers/com.microsoft.Word/Data/WordMat/
+
+#cp  /Library/Application\ support/Microsoft/Office365/User\ content.localized/Add-ins.localized/WordMat/Figurer_spansk.docx ~/Library/Containers/com.microsoft.Word/Data/WordMat/
+
