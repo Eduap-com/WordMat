636b15e8bacc67cf6562c6747cf6fd951869d081
Mikael Samsøe Sørensen <eduap@eduap.com>
2020-05-16 06:56:40 +0200

    Update InstallScript.sh

--- Mac/Installer-files/WordMat/InstallScript.sh
+++ Mac/Installer-files/WordMat/InstallScript.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-# Create the temp folder
-#mkdir -p ~/Library/Containers/com.microsoft.word/Data/WordMat/
 
 # the scriptfile for applescripttask
 mkdir -p ~/Library/Application\ Scripts/com.microsoft.Word/
 mv /Applications/WordMat/WordMatScripts.scpt ~/Library/Application\ scripts/com.microsoft.word/
+
+# Create the temp folder
+mkdir -p ~/Library/Containers/com.microsoft.word/Data/WordMat
+chmod 777 ~/Library/Containers/com.microsoft.Word/Data/WordMat
 
 # previously named file from prev version
 #rm /Library/Application\ Support/Microsoft/Office365/User\ Content.localized/Startup.localized/Word/WordMatWinMac.dotm
